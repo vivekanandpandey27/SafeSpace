514fa3bbd535aa35c657cca4227d0f0f3de3b582
vivekanandpandey27 <Vivekanandpdy732@gmail.com>
2026-06-25 21:57:32 +0530

    Port configuration

--- backend/Server.js
+++ backend/Server.js
@@ -9,7 +9,7 @@ import userRouter from './routes/userRoute.js'
 
 // app config
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 8000
 
 // Connect to database (CALL THE FUNCTION)
 connectDB()
@@ -25,19 +25,20 @@ app.use('/api/doctor', doctorRouter)
 app.use("/api/user", userRouter)
 
 
-app.get("/", (req, res) => {
-  res.send("API Working")
-});
+// app.get("/", (req, res) => {
+//   res.send("API Working")
+// });
 
-app.get('/test-db', (req, res) => {
-  const state = mongoose.connection.readyState;
-  // 0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting
-  if (state === 1) {
-    res.send('Database is connected');
-  } else {
-    res.status(500).send('Database is NOT connected');
-  }
-});
+
+// app.get('/test-db', (req, res) => {
+//   const state = mongoose.connection.readyState;
+//   // 0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting
+//   if (state === 1) {
+//     res.send('Database is connected');
+//   } else {
+//     res.status(500).send('Database is NOT connected');
+//   }
+// });
 
 
 app.listen(port, () => console.log(`Server started on PORT:${port}`))
