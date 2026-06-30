b6d755d440b8311c9c19413add68edc7848f6b98
vivekanandpandey27 <Vivekanandpdy732@gmail.com>
2026-06-30 17:20:57 +0530

    Corrected necessary changes in .env

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(`${process.env.MONGODB_URI}/appointy`);
-    console.log("Database Connected");
+    console.log("Database Connected Successfully");
   } catch (error) {
     console.error("Database connection error:", error);
     process.exit(1);
