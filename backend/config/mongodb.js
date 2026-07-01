8ff30327904a371482a7d8913c6a38e617162d9c
vivekanandpandey27 <Vivekanandpdy732@gmail.com>
2026-07-01 15:58:41 +0530

    Changes done

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/appointy`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/SafeSpace`);
     console.log("Database Connected Successfully");
   } catch (error) {
     console.error("Database connection error:", error);
-    process.exit(1);
+    process.exit(1); //Safe exist(force close node.js app) on failure
   }
 };
 
--- backend/middlewares/authAdmin.js
+++ backend/middlewares/authAdmin.js
@@ -4,10 +4,12 @@ import jwt from "jsonwebtoken"
 const authAdmin = async (req, res, next) => {
     try {
         const { atoken } = req.headers
+        
         if (!atoken) {
             return res.json({ success: false, message: 'Not Authorized Login Again' })
         }
         const token_decode = jwt.verify(atoken, process.env.JWT_SECRET)
+        
         if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
             return res.json({ success: false, message: 'Not Authorized Login Again' })
         }
@@ -19,3 +21,5 @@ const authAdmin = async (req, res, next) => {
 }
 
 export default authAdmin;
+
+
