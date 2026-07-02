386fff9a2b8a40469799c192da0ef3f9557f65ee
Samar Mishra <samarashu00@gmail.com>
2026-07-02 17:05:54 +0530

    Added Google Authentication OAuth2.0

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,18 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import AppContextProvider from './context/AppContext.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <AppContextProvider>
-            <App />
-        </AppContextProvider>
-    </BrowserRouter>,
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <BrowserRouter>
+            <AppContextProvider>
+                <App />
+            </AppContextProvider>
+        </BrowserRouter>
+    </GoogleOAuthProvider>
 )
